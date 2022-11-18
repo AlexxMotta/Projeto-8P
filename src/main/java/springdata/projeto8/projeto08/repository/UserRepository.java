e018cf6b6d4c44b6f8013be24d1cf37db437ab7c
Taylher <alexmota@alu.ufc.br>
2022-11-18 20:14:16 -0300

    removendo classes nao utilizadas

--- src/main/java/springdata/projeto8/projeto08/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package springdata.projeto8.projeto08.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import springdata.projeto8.projeto08.model.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
